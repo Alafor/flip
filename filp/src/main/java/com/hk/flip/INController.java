aac9b173af3750811ea049abc3e1fa394bfd8a9f
hk-edu <hk-edu@192.168.3.103>
2019-04-10 17:56:24 +0900

    아직 아무것안했다

--- filp/src/main/java/com/hk/flip/INController.java
+++ filp/src/main/java/com/hk/flip/INController.java
@@ -6,22 +6,25 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import com.hk.flip.service.IMemberService;
+
+
+
+
 @Controller
 public class INController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(INController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	private IMemberService memberService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -35,5 +38,66 @@ public class INController {
 		
 		return "home";
 	}
+	
+	@RequestMapping(value = "/loginform.do", method = RequestMethod.GET)
+	public String loginform(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "LogIn";
+	}
+	
+	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "Main";
+	}
+	
+	@RequestMapping(value = "/signupform.do", method = RequestMethod.GET)
+	public String signupform(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "SignUp";
+	}
+	
+	/*@RequestMapping(value = "/signup.do", method = RequestMethod.GET)
+	public String signup(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		boolean isS = memberService.signupMember();
+		if(isS) {
+			return "redirect:loginform.do";
+		}else{
+			model.addAttribute("msg", "가입실패");
+			return "error";
+		}
+	}*/
 	
 }
