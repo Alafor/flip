3b23ab257ac2f357345f20f7200be9e7eb5cff63
hk-edu <hk-edu@192.168.3.103>
2019-04-12 09:14:16 +0900

    sfd

--- filp/src/main/java/com/hk/flip/daos/IMemberDao.java
+++ filp/src/main/java/com/hk/flip/daos/IMemberDao.java
@@ -10,11 +10,8 @@ public interface IMemberDao {
 
 	public boolean newMember(MemberDto dto);//회원가입
 
-<<<<<<< HEAD
-	public boolean overlappedID(String id);//ajax id값비교
+	
 
-=======
->>>>>>> branch 'master' of https://github.com/Alafor/FLIP.git
 	MemberDto viewMyMember(String id);
 	
 	public MemberDto getTProfile(String member_id);//강사정보가져오기
