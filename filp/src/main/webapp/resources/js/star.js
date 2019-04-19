50787de95f678f6bfbb6988932c4c5683762a1d6
hk-edu <hk-edu@192.168.3.108>
2019-04-19 14:25:27 +0900

    star.js - 메인 리스트 완료 - 서홍민

--- filp/src/main/webapp/resources/js/star.js
+++ filp/src/main/webapp/resources/js/star.js
@@ -1,7 +1,8 @@
+//별점 출력 클릭 트리거
 $(function(){
 	$(".teacherstar").trigger("click");
-
 });
+//별점 출력 
 function gogostar(stars) {
 	var star1 = stars.getAttribute('data-minority');
 	var mino = star1;
@@ -24,6 +25,7 @@ function gogostar(stars) {
 	stars.innerHTML = origin + star
 }
 
+//메인 리스트 아작스 구현
 $(function(){
 	$(".popular-category").on("mouseover",function(){
 		var department = $(this).attr("data-category");
@@ -38,18 +40,30 @@ $(function(){
 				var studyList = categorylist["studyList"];
 				var wantList = categorylist["wantList"];
 				var instructorAjax=$(".active .instructorAjax");
-//				instructorAjax.eq(0).find(".instructor_img").;
-//				instructorAjax.eq(0).find(".category").text("kkkkkk");
-//				alert($(".active").find(".category").eq(0).text("kkkkkk"));
+				var studentAjax=$(".active .studentAjax");
+				var studyAjax=$(".active .studyAjax");
+				
 		for(var i=0;i<classList.length ;i++){
 			instructorAjax.eq(i).find(".category").text(classList[i].class_creator_name);
 			instructorAjax.eq(i).find("h3 > a").text(classList[i].class_name);
 			instructorAjax.eq(i).find("teacherstar").attr("data-minority",classList[i].class_member_rating);
-			instructorAjax.eq(i).find("teacherstar").attr("data-minority")
+			instructorAjax.eq(i).find("teacherstar").attr("data-minority");
 			instructorAjax.eq(i).find(".review").text("("+classList[i].class_review_count+" Review)");
 			instructorAjax.eq(i).find("address").text(classList[i].class_area);
 		};
-	
+		
+		for(var i=0;i<wantList.length;i++){
+			studentAjax.eq(i).find(".category").text(classList[i].class_creator_name);
+			studentAjax.eq(i).find("h3 > a").text(classList[i].class_name);
+			studentAjax.eq(i).find("address").text(classList[i].class_area);
+		};
+		
+		for(var i=0;i<studyList.length;i++){
+			studyAjax.eq(i).find(".category").text(classList[i].class_creator_name);
+			studyAjax.eq(i).find("h3 > a").text(classList[i].class_name);
+			studyAjax.eq(i).find("address").text(classList[i].class_area);
+		};
+		
 		},
 		error: function(data,status,xhr){
 			alert("통신실패");
