8c3b8a5ad3f287dc0cac0b68137f6ba0f27a4213
hk-edu <hk-edu@192.168.3.104>
2019-05-03 17:45:51 +0900

    회원가입 오류수정 및 정규식 체크 -김일남

--- filp/src/main/webapp/WEB-INF/views/SignUp.jsp
+++ filp/src/main/webapp/WEB-INF/views/SignUp.jsp
@@ -114,6 +114,18 @@ function auto_phone( e, oThis ){
 	}
 
 </script>
+<style type="text/css">
+.hidden_input{margin: 0 auto;}
+.hidden_input label { padding: .5em .75em; color: #fff; font-size: inherit; line-height: normal; vertical-align: middle;
+					 background-color: #30e3ca; cursor: pointer; border: 1px solid #ebebeb; border-bottom-color: #e2e2e2; border-radius: .25em; }
+
+.hidden_input input[type="file"] {position: absolute; width: 1px; height: 1px; padding: 0; margin: -1px; overflow: hidden; clip:rect(0,0,0,0); border: 0; }
+
+
+	
+</style>
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 </head>
 <body>
@@ -165,25 +177,25 @@ function auto_phone( e, oThis ){
         <div class="row justify-content-center">
           <div class="col-md-7 mb-5"  data-aos="fade">
           
-          	<form class="p-5 bg-white validate-form" action="signup.do" onsubmit="return checkpw()" method="post" style="border: 2px solid #30e3ca; border-radius: 20px;">
+          	<form class="p-5 bg-white validate-form" action="signup.do" onsubmit="return checkpw()" method="post" enctype="multipart/form-data" style="border: 2px solid #30e3ca; border-radius: 20px;">
           	<input type="hidden" name="member_type"  value="S">
              
  			<div class="row form-group">              
-                <div class="col-md-12 validate-input" data-validate = "이름을 입력해주세요">
+                <div class="col-md-12 validate-input" data-validate = "한글과영문만 사용가능합니다">
                   <label class="text-black" for="name">이름</label> 
                   <input type="text" class="form-control" name="member_name">
                 </div>
               </div>	            
              
               <div class="row form-group">              
-                <div class="col-md-12 validate-input" data-validate = "아이디를 입력해주세요">
+                <div class="col-md-12 validate-input" data-validate = "5~10자의영문이나 숫자가 조합되어야합니다">
                   <label class="text-black" for="id">아이디</label> 
                   <input type="text" id="t_id" class="form-control" name="member_id" onchange="fn_process()">
                 </div>
                 <div id="message" style="margin-left: 20px; color: red;"></div>
               </div>
               <div class="row form-group">              
-                <div class="col-md-12 validate-input" data-validate = "패스워드를 입력해주세요">
+                <div class="col-md-12 validate-input" data-validate = "최소 8자리에 숫자, 문자, 특수문자 각각 1개 이상 포함되야합니다">
                   <label class="text-black" for="password">패스워드</label> 
                   <input type="password" id="Pw" class="form-control" name="member_password" >
                 </div>
@@ -208,11 +220,25 @@ function auto_phone( e, oThis ){
                 </div>
               </div>
           	<div class="row form-group">              
-                <div class="col-md-12 validate-input"  data-validate = "연락처를 입력해주세요">
+                <div class="col-md-12 validate-input"  data-validate = "'010','011','016','017','018'로 시작하는 번호만 사용 가능합니다.">
                   <label class="text-black" for="phone">전화번호</label> 
                   <input type="text" class="form-control" name="member_phone" 
                   onkeyup="auto_phone(event, this)" onkeypress="auto_phone(event, this)" maxlength="13" placeholder="ex)01012345678">
                 </div>
+              </div>
+              
+              <div class="container">     
+    		<label class="text-black" >프로필등록</label>
+    			<div class="row form-group mb-6" style="border: 1px solid #ced4da; border-radius: 10px; ">
+  					<div class="col-md-6"><!--left col--><br>
+                       <div class="text-center testimonial"><figure class="mb-6"><img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class=" avatar img-fluid mb-6"" alt="avatar"><br><br>
+        					<h6><b>프로필</b></h6></div></div>
+        		       <div class="col-md-6 hidden_input" style="text-align:center;margin-top:15%"><p style="color: orange;">업로드할 사진은 가로세로 사이즈가 같은걸로 하자.</p>
+        					<label for="hidden_file">프로필등록</label>
+        						<input type="file" id="hidden_file" class="text-center center-block file-upload"  name="member_profile"></figure>
+     					</div><br>
+              
+              		</div>
               </div>
               
               <div class="row form-group">
@@ -241,7 +267,28 @@ function auto_phone( e, oThis ){
 	<div>
 	<jsp:include page="footer.jsp" />
 	</div>
-	
+		<script type="text/javascript">
+	$(document).ready(function() {
+
+	    
+	    var readURL = function(input) {
+	        if (input.files && input.files[0]) {
+	            var reader = new FileReader();
+
+	            reader.onload = function (e) {
+	                $('.avatar').attr('src', e.target.result);
+	            }
+	    
+	            reader.readAsDataURL(input.files[0]);
+	        }
+	    }
+	    
+
+	    $(".file-upload").on('change', function(){
+	        readURL(this);
+	    });
+	});
+	</script>
 	<script type="text/javascript" src="resources/js/jquery-3.3.1.min.js"></script>
 	<script src="resources/js/jquery-migrate-3.0.1.min.js"></script>
 	<script src="resources/js/jquery-ui.js"></script>
--- filp/src/main/webapp/resources/js/signup.js
+++ filp/src/main/webapp/resources/js/signup.js
@@ -4,32 +4,22 @@
 
 (function ($) {
     "use strict";
-
-
-    /*==================================================================
-    [ Validate ]*/
     var input = $('.validate-input .form-control');
-
     $('.validate-form').on('submit',function(){
         var check = true;
-
         for(var i=0; i<input.length; i++) {
             if(validate(input[i]) == false){
                 showValidate(input[i]);
                 check=false;
             }
         }
-
         return check;
     });
-
-
     $('.validate-form .form-control').each(function(){
         $(this).focus(function(){
            hideValidate(this);
         });
     });
-
     function validate (input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'member_email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
@@ -42,19 +32,179 @@
             }
         }
     }
-
+ 
     function showValidate(input) {
         var thisAlert = $(input).parent();
-
         $(thisAlert).addClass('alert-validate');
     }
-
     function hideValidate(input) {
         var thisAlert = $(input).parent();
-
         $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
+    } 
+ })(jQuery);   
 
+//아이디 정규식
+(function ($) {
+    "use strict";    
+var input = $('.validate-input .form-control');
+$('.validate-form').on('submit',function(){
+    var check = true;
+    for(var i=0; i<input.length; i++) {
+        if(validate(input[i]) == false){
+            showValidate(input[i]);
+            check=false;
+        }
+    }
+    return check;
+});
+$('.validate-form .form-control').each(function(){
+    $(this).focus(function(){
+       hideValidate(this);
+    });
+});
+ function validate (input) {
+	if($(input).attr('name') == 'member_id') {
+		if($(input).val().trim().match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/) == null) {
+			return false;
+		}
+	}
+	else {
+		if($(input).val().trim() == ''){
+			return false;
+		}
+	}
+}
+function showValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).addClass('alert-validate');
+}
+function hideValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).removeClass('alert-validate');
+	}
 })(jQuery);
+
+//비밀번호 정규식
+(function ($) {
+    "use strict";    
+var input = $('.validate-input .form-control');
+$('.validate-form').on('submit',function(){
+    var check = true;
+    for(var i=0; i<input.length; i++) {
+        if(validate(input[i]) == false){
+            showValidate(input[i]);
+            check=false;
+        }
+    }
+    return check;
+});
+$('.validate-form .form-control').each(function(){
+    $(this).focus(function(){
+       hideValidate(this);
+    });
+});
+function validate (input) {
+	if($(input).attr('type') == 'password'||$(input).attr('name') == 'member_password') {
+		if($(input).val().trim().match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/) == null) {
+			return false;
+		}
+	}
+	else {
+		if($(input).val().trim() == ''){
+			return false;
+		}
+	}
+}
+function showValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).addClass('alert-validate');
+}
+function hideValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).removeClass('alert-validate');
+	}
+})(jQuery);
+
+
+//전화번호 정규식
+(function ($) {
+	"use strict";    
+	var input = $('.validate-input .form-control');
+	$('.validate-form').on('submit',function(){
+		var check = true;
+		for(var i=0; i<input.length; i++) {
+			if(validate(input[i]) == false){
+				showValidate(input[i]);
+				check=false;
+			}
+		}
+		return check;
+	});
+	$('.validate-form .form-control').each(function(){
+		$(this).focus(function(){
+			hideValidate(this);
+		});
+	});
+	  function validate (input) {
+	    	if($(input).attr('name') == 'member_phone') {
+	    		if($(input).val().trim().match(/^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/) == null) {
+	    			return false;
+	    		}
+	    	}
+	    	else {
+	    		if($(input).val().trim() == ''){
+	    			return false;
+	    		}
+	    	}
+	    }
+	function showValidate(input) {
+		var thisAlert = $(input).parent();
+		$(thisAlert).addClass('alert-validate');
+	}
+	function hideValidate(input) {
+		var thisAlert = $(input).parent();
+		$(thisAlert).removeClass('alert-validate');
+	}
+})(jQuery);
+
+//이름 정규식
+(function ($) {
+    "use strict";    
+var input = $('.validate-input .form-control');
+$('.validate-form').on('submit',function(){
+    var check = true;
+    for(var i=0; i<input.length; i++) {
+        if(validate(input[i]) == false){
+            showValidate(input[i]);
+            check=false;
+        }
+    }
+    return check;
+});
+$('.validate-form .form-control').each(function(){
+    $(this).focus(function(){
+       hideValidate(this);
+    });
+});
+function validate (input) {
+	if($(input).attr('name') == 'member_name') {
+		if($(input).val().trim().match(/^[가-힣]{2,4}$/) == null) {
+			return false;
+		}
+	}
+	else {
+		if($(input).val().trim() == ''){
+			return false;
+		}
+	}
+}
+function showValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).addClass('alert-validate');
+}
+function hideValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).removeClass('alert-validate');
+	}
+})(jQuery);
+
